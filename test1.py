b042cd87d5456e69a87277efdd192b4962f953f4
Diima7 <fxarts7@gmail.com>
2019-12-17 22:35:53 +0100

    Update LoL Automation
    -Added Threads
    -Useable with Inputs

--- test1.py
+++ test1.py
@@ -1,10 +1,11 @@
 import pyautogui
-import win32gui
+#import win32gui
 import time
 import keyboard
 import os, threading
 
 dirpath = os.path.dirname(os.path.abspath(__file__))
+champselected = False
 
 def img(imgname): #retruns full imgs path
     imgfolder = dirpath + '\\buttons\\'
@@ -87,17 +88,44 @@ def startgame(firstlane,secondlane):
             pyautogui.click(locate('quesearch'))
             print('Starte Auto Accept..')
             time.sleep(1)
-            auto_accept()
+            
     print('Loop beendet..')
 
 
 
-def bann_pick(champ1,champ2,champ3=None,bann):
+def bann_pick(bann,champ1,champ2=None,champ3=None):
     while keyboard.is_pressed('q') == False:
+        time.sleep(1)
+        print("Search for Banning..")
         if locate('bannchamplabel'):
+            print("Found Bann Label, search textbox..")
             if locate('bannsearch'):
-                ghostclick('bannsearch')
-                pyautogui.wr
+                print("Found Textbox, start banning..")
+                pyautogui.click(locate('bannsearch'))
+                pyautogui.write(bann)
+                pyautogui.move(-440,50)
+                pyautogui.click()           #Need to add 'Submit the bann'
+                time.sleep(1)
+                print('Champion Selected!')
+        
+
+        print("Search for Picking..")
+        if locate('selectchamplabel'):
+            print("Found Pick Label, search textbox..")
+            if locate('champsearch'):               #champsearch is transparent, need another image
+                print("Found Textbox, start picking..")
+                pyautogui.click(locate('champsearch'))
+                time.sleep(1)
+                pyautogui.write(champ1)
+                time.sleep(1)
+                pyautogui.move(-440,50)
+                time.sleep(1)
+                pyautogui.click()
+                time.sleep(1)
+                print('Champion Selected!')
+                champselected = True
+        if locate('selectchamp'):
+            ghostclick('selectchamp')
 
 
 
@@ -105,8 +133,20 @@ def bann_pick(champ1,champ2,champ3=None,bann):
 
 
 
-
+lane1 = input('Erste Lane: ')
+lane2 = input('Zweite Lane: ')
+champ1 = input('Erster Champion: ')
+champ2 = input('Zweiter Champion: ')
+bann = input('Bann Champion: ')
 
 window_title = 'League of Legends'
-hwnd = win32gui.FindWindow(None, window_title)
-win32gui.SetForegroundWindow(hwnd)
+#hwnd = win32gui.FindWindow(None, window_title)
+#win32gui.SetForegroundWindow(hwnd)
+
+t1 = threading.Thread(target=auto_accept)
+t2 = threading.Thread(target=startgame,args=(lane1,lane2))
+t3 = threading.Thread(target=bann_pick,args=(bann,champ1,champ2))
+
+t1.start()
+t2.start()
+t3.start()
