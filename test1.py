48e6763ff170cf79cd7f2f2523c6fb01abf38fc0
Diima7 <fxarts7@gmail.com>
2019-12-17 18:31:43 +0100

    added Client buttons and Code

--- test1.py
+++ test1.py
@@ -1,18 +1,69 @@
 import pyautogui
-import win32gui
-from PIL import Image
+#import win32gui
 import time
 import keyboard
 import os, threading
 
-list = [0,0,0,0]
+dirpath = os.path.dirname(os.path.abspath(__file__))
 
-def changelist():
-    global list
-    list[2] = 200
+def img(imgname): #retruns full imgs path
+    imgfolder = dirpath + '\\buttons\\'
+    return imgfolder + imgname
 
+def locate(imgname): #locate the given img x,y
+    imgfolder = dirpath + '\\buttons\\'
+    fullpath = imgfolder + imgname + '.png'
+    return pyautogui.locateCenterOnScreen(fullpath)
 
-print(list)
-t = threading.Thread(target=changelist)
-t.start()
-print(list)
+def ghostclick(imgname): #Does a invisible click
+    x,y = locate(imgname)
+    mousepos = pyautogui.position()
+    pyautogui.click(x,y)
+    pyautogui.moveTo(mousepos)
+
+def auto_accept():
+    while keyboard.is_pressed('q') == False:
+        time.sleep(3)
+        im = locate('accept')
+        if im:
+            pyautogui.click(im)
+
+def selectlane(givenlane):
+    time.sleep(0.2)
+    while True:
+        lane = locate(givenlane)
+        if lane:
+            pyautogui.click(lane)
+            print(givenlane, 'ausgewählt')
+            return True
+
+def startgame():
+    print('Starte Thread')
+    while keyboard.is_pressed('q') == False:
+        print('Loop beginnt..')
+        
+        if locate('mainplay'):
+            print('Mainplay wird gedrückt')
+            ghostclick('mainplay')
+        time.sleep(1.5)
+        if locate('quecheck'):
+            print('Checkbox wird gedrückt')
+            ghostclick('quecheck')
+        if locate('quesubmit'):
+            print('Beginne Que..')
+            ghostclick('quesubmit')
+        time.sleep(1)
+        if locate('normallabel'):
+            print('Normallabel gefunden')
+            if locate('laneselect'):
+                print('SelectLane wird gedrückt')
+                ghostclick('laneselect')
+        selectlane('mid')
+        time.sleep(1)
+        if locate('laneselect'):
+            ghostclick('laneselect')
+        selectlane('top')
+        print('READY FÜRS GAME :)')
+    print('Loop beendet..')
+
+startgame()
